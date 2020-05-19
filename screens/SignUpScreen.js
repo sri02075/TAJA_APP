d03362e77b1ec19deb012639cbd7845bc1887750
mj <roby8502@naver.com>
2020-05-19 10:22:01 +0900

    signUpScreen create

---
+++ screens/SignUpScreen.js
@@ -0,0 +1,102 @@
+import * as WebBrowser from 'expo-web-browser';
+import * as React from 'react';
+import { Image, Platform, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
+import { Input , Button } from 'react-native-elements';
+import { ScrollView } from 'react-native-gesture-handler';
+import { MonoText } from '../components/StyledText';
+
+export default function SignUpScreen() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.logo}></View>
+      <View style={styles.signUp_area}>
+        <Text style={styles.text_signUp}>SIGN UP</Text>
+      </View>
+      <View style={styles.input_area}>
+        <Input
+          containerStyle={styles.input}
+          placeholder='Email Address'
+          errorStyle={{ color: 'red' }}
+          errorMessage='ENTER A VALID ERROR HERE'
+        />
+        <Input
+          containerStyle={styles.input}
+          placeholder='Password'
+          errorStyle={{ color: 'red' }}
+          errorMessage='ENTER A VALID ERROR HERE'
+        />
+        <Input
+          containerStyle={styles.input}
+          placeholder='Confirm Password'
+          errorStyle={{ color: 'red' }}
+          errorMessage='ENTER A VALID ERROR HERE'
+        />
+      </View>
+      <View style={styles.button_area}>
+        <Button
+          buttonStyle={styles.button}
+          title="SIGN UP"
+        />
+      </View>
+      <View style={styles.forgotPassword_area}>
+        <Text style={styles.text_forgotPassword}>이미 계정이 있으신가요?</Text> 
+         <Text>SIGN_IN.</Text>
+      </View>
+    </View>
+  );
+}
+
+SignUpScreen.navigationOptions = {
+  header: null,
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
+  },
+  logo : {
+    flex: 4,
+    backgroundColor : "red",
+  },
+  signUp_area : {
+    flex: 4,
+    backgroundColor : "blue",
+    justifyContent : "center",
+    alignItems : "center"
+  },
+  input_area : {
+    flex: 5,
+    backgroundColor : "blue",
+    alignItems : "center",
+    paddingLeft : "14.35%",
+    paddingRight : "14.35%",
+  },
+  button_area : {
+    flex: 2,
+    backgroundColor : "yellow",
+    paddingLeft : "14.35%",
+    paddingRight : "14.35%",
+  },
+  forgotPassword_area : {
+    flex: 4,
+    backgroundColor : "purple",
+    alignItems : "center",
+  },
+  text_forgotPassword : {
+    color : "white",
+    fontSize : RFPercentage(2),
+  },
+  text_signUp : {
+    fontSize : RFPercentage(4),
+    color : "yellow",
+  },
+  input : {
+    width : "100%",
+  },
+  button : {
+    height : "90%",
+    borderRadius : 8,
+  }
+});
