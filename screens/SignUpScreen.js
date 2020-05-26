41b1a8016517b561a613eeaa4622dcc6b391e143
mj <roby8502@naver.com>
2020-05-26 17:38:20 +0900

    signUp 화면구성 v0.1

--- screens/SignUpScreen.js
+++ screens/SignUpScreen.js
@@ -10,27 +10,45 @@ import { MonoText } from '../components/StyledText';
 export default function SIgnupScreen() {
   return (
     <View style={styles.container}>
-      <View style={styles.logo}></View>
+      <View style={styles.logo_area}></View>
       <View style={styles.title_area}>
         <Text style={styles.text_signUp}>SIGN UP</Text>
       </View>
       <View style={styles.input_area}>
         <Input
-          containerStyle={styles.input}
+          containerStyle={styles.input_container}
+          inputStyle = {styles.input}
           placeholder='Email Address'
+          placeholderTextColor="#fff"
           errorStyle={{ color: 'red' }}
+          leftIcon={{ type: 'font-awesome',
+                      name: 'envelope', 
+                      color : 'white',
+                      size : 13 }}
           errorMessage=''
         />
         <Input
-          containerStyle={styles.input}
+          containerStyle={styles.input_container}
+          inputStyle = {styles.input}
           placeholder='Password'
+          placeholderTextColor="#fff"
           errorStyle={{ color: 'red' }}
+          leftIcon={{ type: 'font-awesome',
+                      name: 'lock', 
+                      color : 'white',
+                      size : 19 }}
           errorMessage=''
         />
         <Input
-          containerStyle={styles.input}
+          containerStyle={styles.input_container}
+          inputStyle = {styles.input}
           placeholder='Confirm Password'
+          placeholderTextColor="#fff"
           errorStyle={{ color: 'red' }}
+          leftIcon={{ type: 'font-awesome',
+                      name: 'lock', 
+                      color : 'white',
+                      size : 19 }}
           errorMessage=''
         />
       </View>
@@ -38,11 +56,12 @@ export default function SIgnupScreen() {
         <Button
           buttonStyle={styles.button}
           title="SIGN UP"
+          titleStyle={{color:'black',fontWeight : 'bold'}}
         />
       </View>
-      <View style={styles.forgotPassword_area}>
-        <Text style={styles.text_forgotPassword}>이미 계정이 있으신가요?</Text> 
-         <Text>SIGN_IN.</Text>
+      <View style={styles.signIn_area}>
+        <Text style={styles.text_signIn}>이미 계정이 있으신가요?  </Text> 
+         <Text style={{color:'yellow'}}>SIGN_IN.</Text>
       </View>
     </View>
   );
@@ -57,48 +76,53 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#000',
   },
-  logo : {
-    flex: 4,
+  logo_area : {
+    flex: 5,
     backgroundColor : "red",
   },
   title_area : {
-    flex: 4,
+    flex: 3,
     backgroundColor : "blue",
-    justifyContent : "center",
-    alignItems : "center"
+    justifyContent : "flex-start",
+    alignItems : "center",
   },
   input_area : {
     flex: 5,
-    backgroundColor : "white",
+    backgroundColor : "green",
     alignItems : "center",
     paddingLeft : "14.35%",
     paddingRight : "14.35%",
   },
   button_area : {
     flex: 2,
-    backgroundColor : "yellow",
+    backgroundColor : "white",
     paddingLeft : "14.35%",
     paddingRight : "14.35%",
   },
-  forgotPassword_area : {
+  signIn_area : {
     flex: 4,
     backgroundColor : "purple",
     justifyContent : "center",
     flexDirection : "row",
   },
-  text_forgotPassword : {
+  text_signIn : {
     color : "white",
-    fontSize : RFPercentage(2),
+    fontSize : RFPercentage(1.4),
   },
   text_signUp : {
     fontSize : RFPercentage(4),
     color : "yellow",
   },
-  input : {
+  input_container : {
     width : "100%",
   },
+  input : {
+    color : "white",
+    paddingLeft : "10%",
+  },
   button : {
-    height : "90%",
+    height : 55,
     borderRadius : 8,
+    backgroundColor : "yellow"
   }
 });
