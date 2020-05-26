ebaebc7d1d779c7b2d7692833b1db6109cb0e2e5
mj <roby8502@naver.com>
2020-05-26 17:38:02 +0900

    login화면 구성 v0.1

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -2,20 +2,53 @@ import * as WebBrowser from 'expo-web-browser';
 import * as React from 'react';
 import { Image, Platform, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
-import { Input , Button } from 'react-native-elements';
-import { ScrollView } from 'react-native-gesture-handler';
-import { MonoText } from '../components/StyledText';
+import { Input , Button, withTheme } from 'react-native-elements';
 
 export default function LoginScreen() {
   return (
     <View style={styles.container}>
       <View style={styles.title_area}>
-        <Text styles={styles.text_title}>반갑습니다 :)</Text>
-        <Text styles={styles.text_title}>안양 택시</Text>
-        <Text styles={styles.text_title}>같이 탈래요?</Text>
+        <Text style={styles.text_title}>반갑습니다 :)</Text>
+        <Text style={styles.text_title}>안양 택시</Text>
+        <Text style={styles.text_title}>같이 탈래요?</Text>
       </View>
-      <View style={styles.input_area}></View>
-      <View style={styles.button_area}></View>
+      <View style={styles.input_area}>
+        <View style={styles.input_wrapper}>
+          <Input
+              containerStyle={styles.input_container}
+              inputStyle = {styles.input}
+              placeholder='Email Address'
+              placeholderTextColor="#fff" 
+              errorStyle={{ color: 'red' }}
+              leftIcon={{ type: 'font-awesome', name: 'envelope', color : 'white' }}
+              errorMessage=''
+            />
+            <Input
+              containerStyle={styles.input_container}
+              inputStyle = {styles.input}
+              placeholder='Password'
+              placeholderTextColor="#fff"
+              errorStyle={{ color: 'red' }}
+              leftIcon={{ type: 'font-awesome', name: 'lock',color : 'white' }}
+              errorMessage=''
+            />
+          </View>
+          <View style={styles.help_wrapper}>
+            <View style={styles.help_create}>
+              <Text style={styles.text_help}>계정을 생성하시겠어요?</Text>
+            </View>
+            <View style={styles.help_forgot}>
+              <Text style={styles.text_help}>비밀번호를 잊으셨나요?</Text>
+            </View>
+          </View>
+      </View>
+      <View style={styles.button_area}>
+      <Button
+          buttonStyle={styles.button}
+          title="SIGN IN"
+          titleStyle = {{color : 'black',fontWeight : 'bold'}}
+        />
+      </View>
     </View>
   );
 }
@@ -31,17 +64,58 @@ const styles = StyleSheet.create({
   },
   title_area : {
     flex : 8,
-    backgroundColor : "red",
-    justifyContent : "center",
+    /* backgroundColor : "red", */
+    justifyContent : "flex-end",
+    paddingBottom : "4%",
     alignItems : "flex-start",
-    paddingLeft : "10%"
+    paddingLeft : "14.35%"
   },
   input_area : {
     flex : 5,
-    backgroundColor : "blue"
+    /* backgroundColor : "blue", */
+    paddingLeft : "14.35%",
+    paddingRight : "14.35%",
   },
   button_area : {
     flex : 6,
-    backgroundColor : "yellow"
+    paddingLeft : "19.35%",
+    paddingRight : "19.35%",
+  },
+  text_title : {
+    fontSize : RFPercentage(4.7),
+    color : "white",
+  },
+  help_wrapper : {
+    flex : 1,
+    /* backgroundColor : "green", */
+    flexDirection : "row",
+  },
+  help_create : {
+    flex:1,
+    textAlign : "left",
+  },
+  help_forgot : {
+    flex:1,
+    alignItems : "flex-end"
+  },
+  text_help : {
+    fontSize : RFPercentage(1.5),
+    color : "white",
+  },
+  input_wrapper : {
+    flex : 3,
+    justifyContent : "flex-end",
+  },  
+  input_container : {
+    width : "100%",
+  },
+  input : {
+    color : "white",
+    paddingLeft : "10%",
+  },
+  button : {
+    backgroundColor : "yellow",
+    borderRadius : 8,
+    height : 55,
   }
 });
